46ee7d51701e1cdff146917ed6c9c4bc9a07fded
deveshdatwani <datwanidevesh@gmail.com>
2021-07-17 17:38:40 +0530

    Looks like it

--- js/confetti.js
+++ js/confetti.js
@@ -392,3 +392,21 @@ window.addEventListener('resize', function(event){
 confetti.resize();
 });
 });
+
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
